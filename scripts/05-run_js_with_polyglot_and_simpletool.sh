55f43271f67124e796c4a423d191767ff206aec1
David Johnston <dwtj@dwtj.me>
2020-05-08 12:07:22 -0700

    Enable `truffle.class.path.trace` in script
    This system property is useful for troubleshooting. When enabled, the
    list of files found on the truffle class path is printed to stdout.
    
    Also reformatted this script to make the `polyglot` command invocation
    span multiple lines, making it easier to read.

--- scripts/05-run_js_with_polyglot_and_simpletool.sh
+++ scripts/05-run_js_with_polyglot_and_simpletool.sh
@@ -4,4 +4,9 @@ PROJECT_ROOT_DIR=$(pwd)
 SIMPLETOOL_JAR="${PROJECT_ROOT_DIR}/external/simpletool/target/simpletool-20.0.0.jar"
 
 # Q(dwtj): What does `--jvm` do? It seems to be required.
-polyglot --jvm "--vm.Dtruffle.class.path.append=${SIMPLETOOL_JAR}" --simple-code-coverage --file js:srcs/js/hello.js
+polyglot \
+    --jvm \
+    --vm.Dtruffle.class.path.trace=true \
+    "--vm.Dtruffle.class.path.append=${SIMPLETOOL_JAR}" \
+    --simple-code-coverage \
+    --file js:srcs/js/hello.js
